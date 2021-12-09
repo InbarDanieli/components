97f35fe1e849fc98ff0dcd62dfa3b51cc52d434b
Inbar Danieli <inbar0308@gmail.com>
2021-12-09 14:45:19 +0200

    Feat(to-react): Added Search-Bar component

--- to-react/src/App.js
+++ to-react/src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import AutoInput from './components/auto-input/AutoInput';
+import SearchBar from './components/SearchBar';
 
 function App() {
   return (
     <div className="App">
-      <AutoInput/>
+      {/* <AutoInput/> */}
+      <SearchBar items={["cat", "dog", "music", "shopping", "hair brush", "hello world", "pop"]} />
      </div>
   );
 }
